5fe14842e5d0c217eaa0f9dbb41e41c93ae641b7
kahunga <samkahunga65@gmail.com>
2024-01-08 21:49:24 +0300

    added future method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ExecutorService threadpool = Executors.newCachedThreadPool();
+        ListeningExecutorService lExecService = MoreExecutors.listeningDecorator(threadpool);
+
+        ListenableFuture<Integer> callback = lExecService.submit(()->{
+            try {
+                System.out.println("w");
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("z");
+                return 2;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+        lExecService.shutdown();
     }
 }
